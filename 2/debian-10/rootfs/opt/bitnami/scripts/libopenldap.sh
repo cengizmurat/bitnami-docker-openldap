cc507aceef542e300b8e539e2e6e34000fc92c71
Murat Cengiz <mcengiz240296@gmail.com>
2020-08-19 17:49:09 +0200

    Fix typo

--- 2/debian-10/rootfs/opt/bitnami/scripts/libopenldap.sh
+++ 2/debian-10/rootfs/opt/bitnami/scripts/libopenldap.sh
@@ -298,7 +298,7 @@ EOF
         info "Using custom tree file"
         export LDAP_DEFAULT_TREE_PATH="${LDAP_CUSTOM_TREE_FILE}"
     fi
-    info "Creating LDAP default tree at ${LDAP_DEFAULT_TREE_PATH}"
+    info "Creating LDAP default tree from ${LDAP_DEFAULT_TREE_PATH}"
     debug_execute ldapadd -f "${LDAP_DEFAULT_TREE_PATH}" -H "ldapi:///" -D "$LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASSWORD"
 }
 
